6cf20dd740bbd3634c3cfbc8a7106234c1a8abe9
UjjwalMandal2119 <157363081+UjjwalMandal2119@users.noreply.github.com>
2026-01-03 08:59:35 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class fibonacci {
+    public static void main(String args[]){
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter the number");
+     int n =sc.nextInt();
+     for(int i=0;i<n;i++){
+        System.out.println(fib(i));
+     }
+     
+    }
+    public static int fib(int n){
+        if(n==0||n==1){
+            return n;
+            
+        }else{
+            return fib(n-1) + fib(n-2);
+
+        }
+    }
+}
