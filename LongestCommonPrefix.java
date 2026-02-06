b42c045f360e91f81f9355313905b1c84e6efd66
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-06 09:40:20 +0530

    learning

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+    public static void main(String args[]){
+        String [] strs ={"flower","flow","flight"};
+        System.out.println(LongestCommonPrefix1(strs));
+
+    }
+    public static String LongestCommonPrefix1(String [] strs){
+        Arrays.sort(strs);
+        String first=strs[0];
+        String last =strs[strs.length-1];
+        int index =0;
+        while(index<first.length()){
+            if(first.charAt(index)==last.charAt(index)){
+                index++;
+            }else{
+                break;
+            }
+        }
+        return index==0?"":first.substring(0,index);
+
+    }
+}
